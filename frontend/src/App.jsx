d5fbfff8242934666e8c8f133a3c2314365a39a8
OriHdzCodes <annaisa203@gmail.com>
2025-10-02 11:33:25 -0600

    pregunta dos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const App = () => {
   const [posX, setPosX] = useState(6);
   const [posY, setPosY] = useState(8);
 
+  let matrix = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+  
   useEffect(() => {
     const interval = setInterval(() => {
       fetch("http://localhost:8000/run")
       .then(res => res.json())
       .then(res => {
         setPosX(res.agents[0].pos[0]-1);
-        setPosX(res.agents[0].pos[1]-1);
+        setPosY(res.agents[0].pos[1]-1);
       });
     }, 1000);
 
       return () => clearInterval(interval);
-  }, [posX, posY]);
-  
+  }, []);
+
   return (
     <div>
-      <svg width="800" height="500" style={{backgroundColor: "lightgray"}} xmlns="http://www.w3.org/2000/svg">
-        <image x={255 + 25 * posX} y={9 + 25 * posY} href="ghost.png"/>
+      <svg width="800" height="500" xmlns="http://www.w3.org/2000/svg">
+      {
+        matrix.map((row, rowidx) =>
+          row.map((value, colidx) =>
+            <rect x={250 + 25 * rowidx} y={5 + 25 * colidx} width={25} height={25} fill={value == 1 ? "lightgray" : "gray"}/>
+      ))
+      }
+      <image x={255 + 25 * posX} y={9 + 25 * posY} href="ghost.png"/>
       </svg>
     </div>
   );
